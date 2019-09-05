2d70d6b6f8cb223160d2d1c8965a86f0c8eb5202
汐榊少爷 <xmh.yxw@gmail.com>
2019-09-05 17:25:12 +0800

    添加

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+echo 开始发布文件......
+ng build --prod --aot
+pause
---
+++ src/app/common/data-source/requestApis/ssr-api.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from '../../providers/http.service';
+import {Observable} from 'rxjs';
+import {APP_SERVE_URL} from '../../providers/globalData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SsrApiService {
+
+  constructor(private http: HttpService) {
+  }
+
+  /**
+   * 获得ssr
+   */
+  public getSsr() {
+    return new Observable(observer => {
+      this.http.get(APP_SERVE_URL + 'api/ssr?type=freess').subscribe(req => {
+        if (req.error === 200) {
+          observer.next(req);
+        }
+      });
+    });
+
+
+  }
+}
