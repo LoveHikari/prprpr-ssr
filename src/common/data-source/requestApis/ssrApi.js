8ccf4450cd8416621fd9294f0b55d97a8c066bc1
汐榊少爷 <xmh.yxw@gmail.com>
2019-11-19 19:18:17 +0800

    修改了接口

--- src/common/data-source/requestApis/ssrApi.js
+++ src/common/data-source/requestApis/ssrApi.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 let ssrApi = {
     getSsr: function (pageIndex, pageSize, groups = "") {
         const promise = new Promise(resolve => {
-            Axios.get('https://api.yayoo.tk/api/v1/Ssr',
+            Axios.get('https://api.prprpr.ml/api/v1/Ssr',
                 {
                     params: {pageIndex, pageSize, groups}
                 }).then(req => {
